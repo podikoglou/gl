7a4908907e7b64a9129a52986604c0e20ab2d22f
Alex <alex.podikoglou@gmail.com>
2025-05-12 15:41:38 +0300

    feat(nn): initialize weights and biases

--- nn.c
+++ nn.c
@@ -1,5 +1,35 @@
 #include "nn.h"
+#include <math.h>
+#include <stdlib.h>
 
-void nn_init(nn *nn) {}
+float nn_random_weight_xavier() {
+  return (rand() / (float)RAND_MAX - 0.5f) * 2.0f * sqrt(2.0f / (9 + 1));
+}
+
+void nn_init(nn *nn) {
+  /* initialize first layer */
+
+  /* weights */
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 4; j++) {
+      nn->weights_1[i][j] = nn_random_weight_xavier();
+    }
+  }
+
+  /* biases */
+  for (int i = 0; i < 4; i++) {
+    nn->biases_1[i] = 0.0;
+  }
+
+  /* initialize second layer */
+
+  /* weights */
+  for (int i = 0; i < 4; i++) {
+    nn->weights_2[i][0] = nn_random_weight_xavier();
+  }
+
+  /* biases */
+  nn->biases_2[0] = 0.0;
+}
 
 float nn_forward(nn *nn, float x[9]) { return 0.0; }
